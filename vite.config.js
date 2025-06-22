c4f7e082b66ef60608d89897c707677d0ccbbcc3
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-06-22 14:15:54 +0530

    Revert "fix: build and cloudflare mime type error"
    This reverts commit 0e6da4573e9945a7a9bc03901283a178f6d330eb.

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei']
-        }
-      }
-    }
-  }
+  plugins: [react(), tailwindcss(),],
 })
